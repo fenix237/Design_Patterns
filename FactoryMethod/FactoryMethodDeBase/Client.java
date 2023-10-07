afb518192c9e520736fd8dc502ae52ff7145516c
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-08 00:40:30 +0100

    Mise a jour de la classe client

--- FactoryMethod/FactoryMethodDeBase/Client.java
+++ FactoryMethod/FactoryMethodDeBase/Client.java
@@ -1,1 +1,29 @@
 package Design_Patterns.FactoryMethod.FactoryMethodDeBase;
+
+import Design_Patterns.FactoryMethod.FactoryMethodDeBase.Produit.ProduitA;
+import Design_Patterns.FactoryMethod.FactoryMethodDeBase.ProduitFactory.ProduitFactory;
+
+public class Client {
+    public static void main(String[] args){
+        ProduitFactory produitFactory=new ProduitFactory();
+        
+        ProduitA produitA=null;
+
+        System.out.println("\nFACTORY METHODE DE BASE\n");
+
+        System.out.println("\n*****************************");
+
+        produitA=produitFactory.getProduitA(ProduitFactory.TYPE_PRODUITA1);
+        produitA.methodeA();
+
+        produitA=produitFactory.getProduitA(ProduitFactory.TYPE_PRODUITA2);
+        produitA.methodeA();
+
+
+        produitA=produitFactory.getProduitA(4);
+        produitA.methodeA();
+
+        System.out.println("\n*****************************\n");
+    
+    }   
+}
