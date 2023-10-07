6723ed3d5f7c1d2fddc1a3e79eb5b644311e3af0
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-08 00:57:22 +0100

    Implementation de la fabrique

---
+++ FactoryMethod/FactoryMethodModifie/ProduitFactory/ProduitFactory.java
@@ -0,0 +1,32 @@
+package Design_Patterns.FactoryMethod.FactoryMethodModifie.ProduitFactory;
+
+import Design_Patterns.FactoryMethod.FactoryMethodModifie.Produit.ProduitA;
+import Design_Patterns.FactoryMethod.FactoryMethodModifie.Produit.ProduitA1;
+import Design_Patterns.FactoryMethod.FactoryMethodModifie.Produit.ProduitA2;
+import Design_Patterns.FactoryMethod.FactoryMethodModifie.Produit.ProduitA3;
+
+public class ProduitFactory {
+    public static final int TYPE_PRODUITA1=1;
+    public static final int TYPE_PRODUITA2=2;
+    public static final int TYPE_PRODUITA3=3;
+
+    public ProduitA getProduitA(int typeProduit){
+        ProduitA produitA=null;
+        switch (typeProduit) {
+            case TYPE_PRODUITA1:
+                produitA=new ProduitA1();
+                break;
+            case TYPE_PRODUITA2:
+                produitA=new ProduitA2();
+                break;  
+            case TYPE_PRODUITA3:
+                produitA=new ProduitA3();
+                break;      
+        
+            default:
+                throw new IllegalArgumentException("Type de produit inconnu");
+        }
+
+        return produitA;
+    }
+}
