3e6700a0223924abaec9b6fbbc0ebe813cdb4f3c
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-09 21:21:18 +0100

    implementation de la classe client

--- AbstractFactory/AbstractFactoryModifie/Client.java
+++ AbstractFactory/AbstractFactoryModifie/Client.java
@@ -2,20 +2,22 @@ package Design_Patterns.AbstractFactory.AbstractFactoryModifie;
 
 import Design_Patterns.AbstractFactory.AbstractFactoryModifie.Produit.ProduitA;
 import Design_Patterns.AbstractFactory.AbstractFactoryModifie.Produit.ProduitB;
+import Design_Patterns.AbstractFactory.AbstractFactoryModifie.Produit.ProduitC;
 import Design_Patterns.AbstractFactory.AbstractFactoryModifie.ProduitFactory.FabriqueAbstraite;
 import Design_Patterns.AbstractFactory.AbstractFactoryModifie.ProduitFactory.FabriqueConcrete1;
 import Design_Patterns.AbstractFactory.AbstractFactoryModifie.ProduitFactory.FabriqueConcrete2;
-import Design_Patterns.AbstractFactory.AbstractFactoryModifie.ProduitFactory.FabriqueConcrete3;
+
 
 public class Client {
     public static void main(String[] args){
 
         FabriqueAbstraite fabriqueConcrete1=new FabriqueConcrete1();
         FabriqueAbstraite fabriqueConcrete2=new FabriqueConcrete2();
-        FabriqueAbstraite fabriqueConcrete3=new FabriqueConcrete3();
+       
 
         ProduitA produitA=null;
         ProduitB produitB=null;
+        ProduitC produitC=null;
 
         System.out.println("\nFABRIQUE ABSTRAITE MODIFIE\n");
 
@@ -29,6 +31,9 @@ public class Client {
         produitB=fabriqueConcrete1.createProduitB();
         produitB.methodeB();
 
+        produitC=fabriqueConcrete1.createProduitC();
+        produitC.methodeC();
+
         System.out.println("\n*****************************");
 
         System.out.println("\nUtilisation de la 2eme fabrique concrete\n");
@@ -38,17 +43,10 @@ public class Client {
         produitB=fabriqueConcrete2.createProduitB();
         produitB.methodeB();
 
-        System.out.println("\n*****************************");
-
-        System.out.println("\nUtilisation de la 3eme fabrique concrete\n");
-        produitA=fabriqueConcrete3.createProduitA();
-        produitA.methodeA();
-
-        produitB=fabriqueConcrete3.createProduitB();
-        produitB.methodeB();
+        produitC=fabriqueConcrete2.createProduitC();
+        produitC.methodeC();
 
         System.out.println("\n*****************************\n");
 
     }
 }
-
