c70b7e72f8cd304c211150643b3af8578801b1bb
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-28 00:23:26 +0100

    implementation de l'adapteur

--- Adapter/AdapterExemple2DeBase/Adapteur/DListImpStack.java
+++ Adapter/AdapterExemple2DeBase/Adapteur/DListImpStack.java
@@ -1,5 +1,23 @@
 package Design_Patterns.Adapter.AdapterExemple2DeBase.Adapteur;
 
-public class DListImpStack {
+import Design_Patterns.Adapter.AdapterExemple2DeBase.Adapte.DList;
+import Design_Patterns.Adapter.AdapterExemple2DeBase.Interface.Stack;
+import Design_Patterns.Adapter.AdapterExemple2DeBase.Objets.Object;
+
+public class DListImpStack extends DList implements Stack{
+
+    DList dlist=new DList();
+    public void push(Object o){
+        dlist.insertTail(o);
+    }
+
+    public Object pop(){
+        return dlist.removeTail();
+    }
+
+    public Object top(){
+        return dlist.getTail();
+
+    }
     
 }
