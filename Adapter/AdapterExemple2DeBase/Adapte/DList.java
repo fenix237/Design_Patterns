6de3705a96b3c5309f4c58996ca0eeaca1770bc0
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-28 00:34:53 +0100

    implementation du client

--- Adapter/AdapterExemple2DeBase/Adapte/DList.java
+++ Adapter/AdapterExemple2DeBase/Adapte/DList.java
@@ -6,33 +6,38 @@ import Design_Patterns.Adapter.AdapterExemple2DeBase.Objets.Object;
 public class DList {
 
     public void insert(Dnode pos , Object o ){
+        System.out.println("inserer dnode et l'ojet");
 
     }
 
     public void remove(Dnode pos){
-
+        System.out.println("Retirer Dnode");
     }
     
     public void insertHead(Object o ){
-
+        System.out.println("Inserer tete");
     }
 
     public void insertTail(Object o){
-
+        System.out.println("Inserer queue");
     }
 
     public Object removeHead(){
+        System.out.println("Sortie de tete");
         return new Object();
     }
 
     public Object removeTail(){
+        System.out.println("Sortie de queue");
         return new Object();
     }
 
     public Object getHead(){
+        System.out.println("Afficher tete");
         return new Object();
     }
     public Object getTail(){
+        System.out.println("Afficher queue");
         return new Object();
     }
 }
---
+++ Adapter/AdapterExemple2DeBase/Client.java
@@ -0,0 +1,8 @@
+package Design_Patterns.Adapter.AdapterExemple2DeBase;
+
+public class Client {
+    public static void main(String[] args){
+
+        System.out.println("PATRON ADAPTER Cas de L'exemple 2");
+    }
+}
