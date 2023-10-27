9ba7b78be3c3bf6469e3685a4abec2ba30d82c69
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-27 23:55:29 +0100

    implementation de la dliste

---
+++ Adapter/AdapterExemple2DeBase/Adapte/DList.java
@@ -0,0 +1,38 @@
+package Design_Patterns.Adapter.AdapterExemple2DeBase.Adapte;
+
+import Design_Patterns.Adapter.AdapterExemple2DeBase.Objets.Dnode;
+import Design_Patterns.Adapter.AdapterExemple2DeBase.Objets.Object;
+
+public class DList {
+
+    public void insert(Dnode pos , Object o ){
+
+    }
+
+    public void remove(Dnode pos){
+
+    }
+    
+    public void insertHead(Object o ){
+
+    }
+
+    public void insertTail(Object o){
+
+    }
+
+    public Object removeHead(){
+        return new Object();
+    }
+
+    public Object removeTail(){
+        return new Object();
+    }
+
+    public Object getHead(){
+        return new Object();
+    }
+    public Object getTail(){
+        return new Object();
+    }
+}
