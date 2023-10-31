0d48a6cbec2670f7f9ae55b35266eaedb6bf4a89
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-31 13:17:51 +0100

    mise a jour du client

--- Adapter/AdapterExemple2Modifie/Client.java
+++ Adapter/AdapterExemple2Modifie/Client.java
@@ -7,7 +7,7 @@ import Design_Patterns.Adapter.AdapterExemple2Modifie.Objet.Object;
 public class Client {
     public static void main(String[] args){
 
-        System.out.println("PATRON ADAPTER Cas de L'exemple 2 modifie");
+        System.out.println("PATRON  ADAPTER Cas de L'exemple 2 modifie");
         Object object1 = new Object();
         Stack stack = new DListImpStack();
         object1.print();
