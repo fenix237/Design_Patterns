e814898cea0046a2ead6317291da4469995bbe80
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-31 13:05:48 +0100

    implementation de l'objet

--- Adapter/AdapterExemple2Modifie/Objet/Object.java
+++ Adapter/AdapterExemple2Modifie/Objet/Object.java
@@ -3,7 +3,7 @@ package Design_Patterns.Adapter.AdapterExemple2Modifie.Objet;
 
 public class Object {
     public void print(){
-        System.out.println("Instance de l'objet: ");
+        System.out.println("Instance  de l'objet: ");
         System.out.println(this.toString());
         
     }
