c9009898e50d532f55b06c3fd9809b3d0dfdc993
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-31 12:44:13 +0100

    mise a jour du client

---
+++ Adapter/AdapterExemple2Modifie/Adapte/DList.java
@@ -0,0 +1,43 @@
+package Design_Patterns.Adapter.AdapterExemple2Modifie.Adapte;
+import Design_Patterns.Adapter.AdapterExemple2Modifie.Objet.Object;
+import Design_Patterns.Adapter.AdapterExemple2Modifie.Objet.Dnode;
+
+public abstract class DList {
+
+    public void insert(Dnode pos , Object o ){
+        System.out.println("Inserer dnode et l'objet");
+
+    }
+
+    public void remove(Dnode pos){
+        System.out.println("Retirer Dnode");
+    }
+    
+    public void insertHead(Object o ){
+        System.out.println("Inserer tete");
+    }
+
+    public void insertTail(Object o){
+        System.out.println("Inserer queue");
+    }
+
+    public Object removeHead(){
+        System.out.println("Sortie de tete");
+        return new Object();
+    }
+
+    public Object removeTail(){
+        System.out.println("Sortie de queue");
+        return new Object();
+    }
+
+    public Object getHead(){
+        System.out.println("Afficher tete");
+        return new Object();
+    }
+    public Object getTail(){
+        System.out.println("Afficher queue");
+        return new Object();
+    }
+}
+
