033f89620d21c2a10d21a64a6ba639ae44d14409
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-31 10:56:54 +0100

    implementation du dnode

--- Adapter/AdapterExemple2DeBase/Objets/Dnode.java
+++ Adapter/AdapterExemple2DeBase/Objets/Dnode.java
@@ -2,7 +2,7 @@ package Design_Patterns.Adapter.AdapterExemple2DeBase.Objets;
 
 public class Dnode {
     public void print(){
-        System.out.println("Instance du dnode: ");
+        System.out.println("Instance du dnode:");
         System.out.println(this.toString());
         
     }
