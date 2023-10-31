e659fd0db10e0af44971f82f95889f579b00d55a
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-10-31 13:02:03 +0100

    implementation  de l'interface

--- Adapter/AdapterExemple2Modifie/Interface/Stack.java
+++ Adapter/AdapterExemple2Modifie/Interface/Stack.java
@@ -2,7 +2,7 @@ package Design_Patterns.Adapter.AdapterExemple2Modifie.Interface;
 import Design_Patterns.Adapter.AdapterExemple2Modifie.Objet.Object;
 public interface Stack {
 
-    void push(Object o);
+    void  push(Object o);
     Object pop();
     Object top();
  
