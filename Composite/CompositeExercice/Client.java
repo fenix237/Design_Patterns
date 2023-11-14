3fac508009fe363d7e93b666d1cb53ce05d5c839
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-11-14 14:20:42 +0100

    implemention du client

--- Composite/CompositeExercice/Client.java
+++ Composite/CompositeExercice/Client.java
@@ -11,9 +11,9 @@ public class Client {
     public static void main(String[] args) {
         System.out.println("\n\nPatron composite exercice\n\n");
 
-        System.out.println("****************************************");
+        System.out.println("\n****************************************\n");
 
-        System.out.println("Creation d'un fichier PDF\n");
+        System.out.println("\nCreation d'un fichier PDF\n");
 
         Fichier fichierPdf = new FichierPDF("first_File_Pdf");
         fichierPdf.decrire();
@@ -23,6 +23,8 @@ public class Client {
         Fichier fichierTxt = new FichierTXT("first_file_Txt");
         fichierTxt.decrire();
 
+        System.out.println("\n**************************\n");   
+
         System.out.println("\nInsertion des fichier dans un dossier\n");
 
         ElementSystem dossier = new Dossier("Les fichiers");
@@ -31,13 +33,29 @@ public class Client {
         
         dossier.ajouter(fichierTxt);
 
-        System.out.println("\n**************************\n");    
+        System.out.println("\n**************************\n"); 
+        System.out.println("\nDescription du dossier\n");   
         dossier.decrire();
 
-        
-        System.out.println("Obtention de l'elemet i");
+
+        System.out.println("\n**********************************\n");
+
+        System.out.println("\nObtention de l'elemet i\n");
         ElementSystem file = dossier.obtenir(1);
         file.decrire();
+
+        System.out.println("\n****************************\n");
+       System.out.println("\nCreation d'un sous dosier et insertion dans le dosssier principal\n");
+
+        ElementSystem sousdossier = new Dossier("sous dossier");
+        dossier.ajouter(sousdossier);
+
+        System.out.println("\n*******************************************\n");
+
+        System.out.println("\nDerniere description du dossier\n");
+        dossier.decrire();
+
+        System.out.println("\n\n*******************************************************\n\n");
     }
     
 }
