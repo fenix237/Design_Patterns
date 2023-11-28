b29a3640e27ff3a6b6840843e4077b4f438a89ad
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-11-28 06:25:22 +0100

    creation de l'implementaion concrete

--- Bridge/BridgeExempleModifie/ConcreteImplementor/RedColor.java
+++ Bridge/BridgeExempleModifie/ConcreteImplementor/RedColor.java
@@ -3,6 +3,7 @@ package Design_Patterns.Bridge.BridgeExempleModifie.ConcreteImplementor;
 import Design_Patterns.Bridge.BridgeExempleModifie.Implementor.Color;
 
 public class RedColor implements Color {
+    
     public void fillColor() {
         
         System.out.println("red color");
