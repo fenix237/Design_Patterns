545f9734694ed6f258c70da9e2fb0f9767ef4d06
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-11-28 05:51:54 +0100

    implementation de la classe main

--- Bridge/BridgeExemple/BridgeDesignPatternMain.java
+++ Bridge/BridgeExemple/BridgeDesignPatternMain.java
@@ -8,6 +8,7 @@ import Design_Patterns.Bridge.BridgeExemple.RefineAbstraction.Rectangle;
 
 public class BridgeDesignPatternMain {
     public static void main(String[] args) {
+        
         Shape s1 = new Rectangle(new RedColor());
         s1.colorIt();
         Shape s2 = new Circle(new BlueColor());
