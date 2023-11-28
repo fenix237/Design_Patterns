f3dc3272d7f9db8fe0756eedc250c066ab5d92a3
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2023-11-28 11:10:40 +0100

    creation du decorateur

--- Decorator/DecoratorExemple/Decorateur/DecorateurFenetre.java
+++ Decorator/DecoratorExemple/Decorateur/DecorateurFenetre.java
@@ -6,7 +6,21 @@ public abstract class DecorateurFenetre implements Fenetre {
 
     protected Fenetre fenetreDecoree;
     public DecorateurFenetre(Fenetre fenetre){
-        
+
+        this.fenetreDecoree=fenetre;
+
     }
+
+    @Override
+    public void dessiner(){
+        fenetreDecoree.dessiner();
+    }
+
+    @Override
+    public void decrire(){
+        fenetreDecoree.decrire();
+    }
+    
+    public abstract void dessinerCurseur();
     
 }
